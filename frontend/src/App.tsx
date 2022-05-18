1c86a0eddc9bd8a6696e11f48473041945e398ea
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2022-05-18 15:04:33 +0700

    request test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export type weatherType = {
+  date: string,
+  temperatureC: number,
+  temperatureF: number,
+  summary: string
+}
+
 function App() {
+  const [data, setData] = useState<weatherType[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7085/weatherforecast").then(res => {
+      console.log("recieved res: ", res);
+      return (res.json() as any) as weatherType[];
+    }).then(_data => {
+      console.log("recieved data: ", _data);
+      setData(_data);
+    })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          data:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {data?.map((el, index) => {
+          return (<p key={index}>{el.date}  {el.summary}  ({el.temperatureC})</p>);
+        })}
       </header>
     </div>
   );
