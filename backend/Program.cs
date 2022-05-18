c5c115c0623e5c48e6de7158b15bf19e08229b22
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2022-05-18 15:04:00 +0700

    disable CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,11 +4,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
